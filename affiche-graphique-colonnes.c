91b2f450fa166aa260cb548555dd7e074a3081d8
adriengui <55453387+adriengui@users.noreply.github.com>
2022-11-18 09:27:53 +0100

    Update affiche-graphique-colonnes.c

--- affiche-graphique-colonnes.c
+++ affiche-graphique-colonnes.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 
-int main(){
+int main() {
 	int tab[6]={5,9,7,3,14,8};
-	int len=sizeof(tab)/sizeof(tab[0]);
-	int a=0;
-	for(int p=1;p<len;p++) if(tab[p]>tab[p-1]) a=tab[p];
-	for(int x=a;x>0;x--){
-		for(int y=0;y<len;y++){
-			if(x<=tab[y]) printf("* ");
+	int len=sizeof(tab)/sizeof(int), v=tab[0], i, j;
+	
+	for(i=1;i<len;i++) 
+		if(tab[i]>v) 
+			v=tab[i];
+	
+	for(i=v;i>0;i--) {
+		for(j=0;j<len;j++) {
+			if(i<=tab[j]) printf("* ");
 			else printf("  ");
 		}
 		printf("\n");
